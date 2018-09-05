e0e2a7d8808a0b36bc220d43d82c8cd430070bb3
Daniel Lanna <daniel.lanna@bloomingdales.com>
2018-09-05 11:52:18 -0300

    Mutable Solution

--- src/redux/reducers/MutableUserReducer.js
+++ src/redux/reducers/MutableUserReducer.js
@@ -14,11 +14,38 @@ export default (state = INITIAL_STATE, action) => {
     const { type, payload } = action;
     switch (type) {
         case USER.MUTABLE_UPDATE_DEEP:
-            state.foo.bar = payload;
-            return state;
+            // Crap! Only foo is copied, baz wont!
+            // return Object.assign({}, state, {
+            //     location: 'US',
+            //     foo: {
+            //         bar: payload
+            //     }
+            // });
+
+            // All other interests are copied
+            // Object.assign
+            // return Object.assign({}, state, {
+            //     foo: Object.assign({}, state.foo, {
+            //         bar: payload
+            //     })
+            // });
+
+            // SPREAD
+            return {
+                ...state,
+                foo: {
+                    ...state.foo,
+                    bar: payload,
+                }
+            }
         case USER.MUTABLE_SET_NAME:
-            state.name = payload;
-            return state;
+            // Object.assign
+            // return Object.assign({}, state, {
+            //     name: payload,
+            // });
+
+            // SPREAD
+            return { ...state, name: payload }
         default:
             return state;
     }
