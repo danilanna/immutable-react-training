bd51eae38a49939463d151a3955fda61392c7355
Daniel Lanna <daniel.lanna@bloomingdales.com>
2018-09-14 09:57:42 -0300

    Immutable Methods

--- src/redux/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import MutableUserReducer from "./MutableUserReducer";
-import ImmutableUserReducer from "./ImmutableUserReducer";
-
-export default {
-  mutable: MutableUserReducer,
-  immutable: ImmutableUserReducer,
-};
--- src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers, createStore } from 'redux';
-import reducers from './reducers';
-
-const reducer = combineReducers(reducers);
-const store = createStore(reducer);
-
-export default store;
